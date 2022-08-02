3c773d82089643c590d1ff8a9325797f0cc24ba3
Andrew Tsai <a168tsai@gmail.com>
2022-08-02 15:06:06 -0600

    Add statements

--- variables.rb
+++ variables.rb
@@ -5,3 +5,11 @@ starting_age = 35
 amount_paid = 1200.00
 
 deposit_paid = true
+deposit_paid # true
+
+deposit_paid = false
+deposit_paid # false
+
+puts "Welcome to Turing"
+
+puts "This will be a 7 months of roller coaster ride"
